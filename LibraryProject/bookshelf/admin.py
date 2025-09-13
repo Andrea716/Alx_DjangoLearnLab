1fc78bdb3a03711f1eab0c69b979a59388988f63
Andrea716 <andreavisagie07@gmail.com>
2025-09-13 12:53:18 +0200

    Delete LibraryProject directory

--- LibraryProject/bookshelf/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-# Register the Book model
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # columns in the admin list
-    search_fields = ('title', 'author')                    # add search box
-    list_filter = ('publication_year',)                   # filter by publication year
